7450ae4e76699c9bee3ceacdf884e9cc1b698c31
Xiaochen Lu <xiaoclu@ebay.com>
2022-10-10 16:27:02 +0800

    add: backbone initial setup

---
+++ models/backbone.py
@@ -0,0 +1,135 @@
+# ------------------------------------------------------------------------------------
+# Modified from Deformable DETR (https://github.com/fundamentalvision/Deformable-DETR)
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------------------
+# Originated from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# ------------------------------------------------------------------------------------
+
+"""
+Backbone Module
+"""
+import torch
+import torchvision
+import torch.nn.functional as F
+
+from torch import nn
+from typing import Dict, List
+from torchvision.models._utils import IntermediateLayerGetter
+
+from util.misc import NestedTensor, is_main_process
+from .position_encoding import build_position_encoding
+
+
+class FrozenBatchNorm2d(torch.nn.Module):
+    """
+    BatchNorm2d where the batch statistics and the affine parameters are fixed.
+
+    Copy-paste from torchvision.misc.ops with added eps before rqsrt,
+    without which any other models than torchvision.models.resnet[18,34,50,101]
+    produce NaNs.
+    """
+
+    def __init__(self, n, eps=1e-5):
+        super(FrozenBatchNorm2d, self).__init__()
+        self.register_buffer("weight", torch.ones(n))
+        self.register_buffer("bias", torch.zeros(n))
+        self.register_buffer("running_mean", torch.zeros(n))
+        self.register_buffer("running_var", torch.ones(n))
+        self.eps = eps
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        num_batches_tracked_key = prefix + 'num_batches_tracked'
+        if num_batches_tracked_key in state_dict:
+            del state_dict[num_batches_tracked_key]
+
+        super(FrozenBatchNorm2d, self)._load_from_state_dict(
+            state_dict, prefix, local_metadata, strict,
+            missing_keys, unexpected_keys, error_msgs)
+
+    def forward(self, x):
+        w = self.weight.reshape(1, -1, 1, 1) #* move reshapes to the beginning to make it fuser-friendly
+        b = self.bias.reshape(1, -1, 1, 1)
+        rv = self.running_var.reshape(1, -1, 1, 1)
+        rm = self.running_mean.reshape(1, -1, 1, 1)
+        eps = self.eps
+        scale = w * (rv + eps).rsqrt()
+        bias = b - rm * scale
+        return x * scale + bias
+
+
+class BackboneBase(nn.Module):
+
+    def __init__(self, backbone: nn.Module, train_backbone: bool, return_interm_layers: bool):
+        super().__init__()
+        #* if not backbone should be freezed OR layer is not 2/3/4, then freeze it
+        for name, parameter in backbone.named_parameters():
+            if not train_backbone or 'layer2' not in name and 'layer3' not in name and 'layer4' not in name:
+                parameter.requires_grad_(False)
+        
+        if return_interm_layers:
+            return_layers = {"layer2": "0", "layer3": "1", "layer4": "2"}
+            self.strides = [8, 16, 32]
+            self.num_channels = [512, 1024, 2048]
+        else:
+            return_layers = {'layer4': "0"}
+            self.strides = [32]
+            self.num_channels = [2048]
+        self.body = IntermediateLayerGetter(backbone, return_layers=return_layers) #* return_layers -> { <layer_name>: <key_for_feature_dict> }
+
+    def forward(self, tensor_list: NestedTensor):
+        xs = self.body(tensor_list.tensors)
+        out: Dict[str, NestedTensor] = {}
+        for name, x in xs.items():
+            m = tensor_list.mask #* here we need mask for every level of feature backbone returns (interpolate the mask to corresponding size)
+            assert m is not None
+            mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0] #* [None] => unsqueeze(0)
+            out[name] = NestedTensor(x, mask) #* NestedTensor is a self-defined container for feature map & mask, with utility methods provided
+        return out
+
+
+class Backbone(BackboneBase):
+    """ResNet backbone with frozen BatchNorm."""
+    def __init__(self, name: str,
+                 train_backbone: bool,
+                 return_interm_layers: bool,
+                 dilation: bool):
+        norm_layer = FrozenBatchNorm2d
+        backbone = getattr(torchvision.models, name)(
+            replace_stride_with_dilation=[False, False, dilation], #* dilation settings for layer 2/3/4, refer to https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py#L202-L204 for more
+            pretrained=is_main_process(), norm_layer=norm_layer)
+        assert name not in ('resnet18', 'resnet34'), "number of channels are hard coded"
+        super().__init__(backbone, train_backbone, return_interm_layers)
+        if dilation:
+            self.strides[-1] = self.strides[-1] // 2
+
+
+class Joiner(nn.Sequential):
+    def __init__(self, backbone, position_embedding):
+        super().__init__(backbone, position_embedding)
+        self.strides = backbone.strides
+        self.num_channels = backbone.num_channels
+
+    def forward(self, tensor_list: NestedTensor):
+        xs = self[0](tensor_list) #* ResNet backbone forward
+        out: List[NestedTensor] = []
+        pos = []
+        for name, x in sorted(xs.items()):
+            out.append(x)
+
+        # position encoding
+        for x in out:
+            pos.append(self[1](x).to(x.tensors.dtype))
+
+        return out, pos #* feature and positional encodings are matched via List index
+
+
+def build_backbone(args):
+    position_embedding = build_position_encoding(args) #* Init the position encoding network
+    train_backbone = args.lr_backbone > 0
+    return_interm_layers = args.masks or (args.num_feature_levels > 1)
+    backbone = Backbone(args.backbone, train_backbone, return_interm_layers, args.dilation)
+    model = Joiner(backbone, position_embedding)
+    return model
---
+++ test.py
@@ -0,0 +1,48 @@
+import torch
+from util.misc import NestedTensor
+
+from models import backbone
+
+class ArgWrapper:
+    def __init__(self):
+        return
+
+args = ArgWrapper()
+args.hidden_dim = 512
+args.position_embedding = 'sine'
+args.lr_backbone = 0.05
+args.num_feature_levels = 3
+args.backbone = 'resnet50'
+args.dilation = True
+args.masks = None
+
+
+#region Test backbone
+print(f'Backbone test start...')
+
+img = torch.randn((5, 3, 473, 473))                             #* assume batch size = 5
+mask = torch.ones((5, 473, 473))                                #TODO not sure whether this shape is correct
+
+nestedInput = NestedTensor(img, mask)                           #* create nestedTensor for input
+
+backbone_model = backbone.build_backbone(args)
+
+out, pos = backbone_model(nestedInput)
+
+out_shapes = [ele.tensors.shape for ele in out]
+pos_shapes = [ele.shape for ele in pos]
+
+expected_out_shapes = \
+    '[torch.Size([5, 512, 60, 60]), torch.Size([5, 1024, 30, 30]), torch.Size([5, 2048, 30, 30])]'
+expected_pos_shapes = \
+    '[torch.Size([5, 512, 60, 60]), torch.Size([5, 512, 30, 30]), torch.Size([5, 512, 30, 30])]'
+
+assert f'{out_shapes}'==expected_out_shapes, \
+    f'Backbone output shape test failed.\nExpected: {expected_out_shapes};\nGot {out_shapes} instead'
+
+assert f'{pos_shapes}'==expected_pos_shapes, \
+    f'Backbone output shape test failed.\nExpected: {expected_pos_shapes};\nGot {pos_shapes} instead'
+
+print(f'Backbone test complete ✅')
+#endregion Test backbone
+
