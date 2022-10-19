f2d04cbccc50f2c8caf1545a0d17b5cd1ca6e625
Xiaochen Lu <xiaoclu@ebay.com>
2022-10-19 13:13:19 +0800

    fix: fix samples not sent to device while training

--- src/engine.py
+++ src/engine.py
@@ -34,6 +34,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
 
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
     # for _ in metric_logger.log_every(range(len(data_loader)), print_freq, header):
+        samples = samples.to(device)
         outputs = model(samples)
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
